b347539d623e3f739ec2484e425a90a9c3d3d412
Fernando <nandopookey@gmail.com>
2024-10-22 19:01:28 +0700

    22 Oktober 2024

---
+++ resources/views/admin/about/index.blade.php
@@ -0,0 +1,47 @@
+<section class="section">
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="card-body">
+                    <form action="{{ route('about.update', $about->id) }}" class="row g-3 mt-4" method="POST"
+                        enctype="multipart/form-data">
+                        @method('PUT')
+                        @csrf
+                        @if ($errors->any())
+                            <div class="alert alert-danger">
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
+                        <div class="col-md-6">
+                            <label class="form-label">Title</label>
+                            <input type="text" name="title" value="{{ $about->title }}" class="form-control">
+                        </div>
+                        <div class="col-6">
+                            <label class="form-label">Description</label>
+                            <div class="form-floating">
+                                <textarea class="form-control" name="description" id="floatingTextarea" style="height: 100px;">{{ $about->description }}</textarea>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <label class="form-label">Logo</label>
+                            <input type="file" name="logo" multiple class="form-control"
+                                onchange="loadFile(event)">
+                            <img id="output" class="img-fluid mt-2 mb-4"
+                                style="width: 100px; height: 100px; object-fit: cover;" />
+                        </div>
+                        <img src="{{ Storage::url($footerContent->logo ?? '') }}" class="lazyload"
+                            style="width: 155px; height: 180px; object-fit: cover; margin-top: 50px;" alt="image">
+
+                        <div class="text-start">
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ resources/views/user/about/index.blade.php
@@ -0,0 +1,89 @@
+    <!-- bradcam_area_start  -->
+    <div class="bradcam_area breadcam_bg overlay d-flex align-items-center justify-content-center">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="bradcam_text text-center">
+                        <h3>About US</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- bradcam_area_end  -->
+
+    <!-- latest_activites_area_start  -->
+    <div class="latest_activites_area mt-5">
+        <div class=" video_bg_1 video_activite  d-flex align-items-center justify-content-center">
+            <img src="{{ Storage::url($about->logo ?? '') }}" class="lazyload"
+                style="width: 155px; height: 180px; object-fit: cover; margin-top: 50px;" alt="image">
+        </div>
+        <div class="container">
+            <div class="row justify-content-end">
+                <div class="col-lg-7">
+                    <div class="activites_info">
+                        <div class="section_title">
+                            <h3><span>{{ $about->title }}</span></h3>
+                        </div>
+                        <p class="para_1">{{ $about->description }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- latest_activites_area_end  -->
+
+    <!-- counter_area_start  -->
+    {{-- <div class="counter_area pt-120">
+        <div class="container">
+            <div class="counter_bg overlay">
+                <div class="row">
+                    <div class="col-lg-3 col-md-6">
+                        <div class="single_counter d-flex align-items-center justify-content-center">
+                            <div class="icon">
+                                <i class="flaticon-calendar"></i>
+                            </div>
+                            <div class="events">
+                                <h3 class="counter">120</h3>
+                                <p>Finished Event</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="single_counter d-flex align-items-center justify-content-center">
+                            <div class="icon">
+                                <i class="flaticon-heart-beat"></i>
+                            </div>
+                            <div class="events">
+                                <h3 class="counter">120</h3>
+                                <p>Finished Event</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="single_counter d-flex align-items-center justify-content-center">
+                            <div class="icon">
+                                <i class="flaticon-in-love"></i>
+                            </div>
+                            <div class="events">
+                                <h3 class="counter">120</h3>
+                                <p>Finished Event</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="single_counter d-flex align-items-center justify-content-center">
+                            <div class="icon">
+                                <i class="flaticon-hug"></i>
+                            </div>
+                            <div class="events">
+                                <h3 class="counter">120</h3>
+                                <p>Finished Event</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> --}}
+    <!-- counter_area_end  -->
